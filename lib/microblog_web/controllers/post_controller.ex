1a62c103a9f01bb005259795e9af6b9cca2a73d9
djreed <ddavidreed@gmail.com>
2017-10-16 16:57:26 -0400

    Reverse posts on index page

--- lib/microblog_web/controllers/post_controller.ex
+++ lib/microblog_web/controllers/post_controller.ex
@@ -8,7 +8,7 @@ defmodule MicroblogWeb.PostController do
   def index(conn, _params) do
     posts = Blog.list_posts()
 
-    render(conn, "index.html", posts: posts)
+    render(conn, "index.html", posts: Enum.reverse(posts))
 
   end
 
