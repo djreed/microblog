49e0feefa1cd08b82b6c2e915f85639ddbebe0e3
djreed <ddavidreed@gmail.com>
2017-10-16 18:07:02 -0400

    Add hw06 status

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :microblog, MicroblogWeb.Endpoint,
-  http: [port: 4001],
+  http: [port: 4000],
   server: false
 
 # Print only warnings and errors during test
@@ -15,5 +15,5 @@ config :microblog, Microblog.Repo,
   username: "microblog",
   password: "huehuehue",
   database: "microblog_test",
-  hostname: "localhost",
+  hostname: "127.0.0.1",
   pool: Ecto.Adapters.SQL.Sandbox
